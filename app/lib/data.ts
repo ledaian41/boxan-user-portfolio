6c66b4d63014b21d2adeb12836d4fba679c1ef79
An Le <an.le@i-on.net>
2023-12-09 11:46:12 +0700

    test DB connection

---
+++ app/lib/data.ts
@@ -0,0 +1,24 @@
+import { sql } from '@vercel/postgres';
+import { unstable_noStore as noStore } from 'next/cache';
+
+export async function fetchInvoicesPages(query: string) {
+  noStore();
+  try {
+    const count = await sql`SELECT COUNT(*)
+    FROM invoices
+    JOIN customers ON invoices.customer_id = customers.id
+    WHERE
+      customers.name ILIKE ${`%${query}%`} OR
+      customers.email ILIKE ${`%${query}%`} OR
+      invoices.amount::text ILIKE ${`%${query}%`} OR
+      invoices.date::text ILIKE ${`%${query}%`} OR
+      invoices.status ILIKE ${`%${query}%`}
+  `;
+
+    const totalPages = Math.ceil(Number(count.rows[0].count) / 6);
+    return totalPages;
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to fetch total number of invoices.');
+  }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import styles from './page.module.css'
+import {fetchInvoicesPages} from "@/app/lib/data";
 
-export default function Home() {
+export default async function Home() {
+  const totalPages = await fetchInvoicesPages('');
   return (
     <main className={styles.main}>
       Boxan - User Portfolio Landing Page
+      <div>
+        Test DB Connection - {totalPages}
+      </div>
     </main>
   )
 }
